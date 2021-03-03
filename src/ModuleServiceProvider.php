71ac3cfa40551edf9f9c4279fe544dcbdc70547b
Hermann D. Schimpf <hschimpf@hds-solutinons.net>
2021-03-03 17:26:25 -0300

    Register middlewares for module

--- src/ModuleServiceProvider.php
+++ src/ModuleServiceProvider.php
@@ -2,18 +2,28 @@
 
 namespace HDSSolutions\Laravel\Modules;
 
+use Illuminate\Routing\Router;
+
 abstract class ModuleServiceProvider extends \Illuminate\Support\ServiceProvider {
+
+    protected string $middlewaresGroup = 'web';
+
+    protected array $middlewares = [];
 
     /**
     * Publishes configuration file.
     *
     * @return  void
     */
-    public final function boot() {
+    public final function boot(Router $router) {
         // normal boot
         $this->bootEnv();
         // boot for console
         if ($this->app->runningInConsole()) $this->bootCli();
+        // register middlewares
+        foreach ($this->middlewares as $middleware)
+            // register middleware on web group
+            $router->pushMiddlewareToGroup($this->middlewaresGroup, $middleware);
     }
 
     protected function bootEnv():void {}
