bc0ebbc23633205b3e7717f142fe153a1dc2078a
Hermann D. Schimpf <hschimpf@hds-solutions.net>
2021-04-27 18:25:52 -0400

    FIX: Keep collection between calls

--- src/ModulesManager.php
+++ src/ModulesManager.php
@@ -2,17 +2,25 @@
 
 namespace HDSSolutions\Laravel\Modules;
 
+use Illuminate\Support\Collection;
+
 final class ModulesManager {
 
-    private array $seeders_paths = [];
+    public function __construct(
+        private Collection|null $seeders_paths = null
+    ) {
+        $this->seeders_paths = collect();
+    }
 
     public function register(string|array $paths) {
-        // save paths
-        $this->seeders_paths += is_array($paths) ? array_values($paths) : [ $paths ];
+        // foreach paths
+        collect(is_array($paths) ? array_values($paths) : [ $paths ])
+            // push path to collection
+            ->each(fn($path) => $this->seeders_paths->push( $path ));
     }
 
     public function getPaths():array {
-        return $this->seeders_paths;
+        return $this->seeders_paths->toArray();
     }
 
 }
